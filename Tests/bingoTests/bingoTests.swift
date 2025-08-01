da3c0a2e1db634e98a90e719efe166c3e3eabf00
Rick M <rmann@latencyzero.com>
2025-07-31 17:51:53 -0700

    Generate Vapor project.

---
+++ Tests/bingoTests/bingoTests.swift
@@ -0,0 +1,16 @@
+@testable import bingo
+import VaporTesting
+import Testing
+
+@Suite("App Tests")
+struct bingoTests {
+@Test("Test Hello World Route")
+    func helloWorld() async throws {
+        try await withApp(configure: configure) { app in
+            try await app.testing().test(.GET, "hello", afterResponse: { res async in
+                #expect(res.status == .ok)
+                #expect(res.body.string == "Hello, world!")
+            })
+        }
+    }
+}
